e52050feeb9609ae116043e1c6bd702bc36d5864
Camilo Velasquez A <cvelas31@gmail.com>
2020-07-24 22:03:56 -0500

    Push with plt and secedgar working

--- api/app.py
+++ api/app.py
@@ -2,7 +2,6 @@ import sys
 
 from flask import Flask
 from flask_restful import abort, Api
-from resources import PlotGenerator
 import finviz
 import pandas as pd
 import numpy as np
@@ -10,6 +9,9 @@ import quandl
 import yfinance as yf
 import matplotlib.pyplot as plt
 import os
+
+from resources import PlotGenerator
+from api_utils import read_all_form_4, create_ticker2name, create_combined_data
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
@@ -21,46 +23,10 @@ api = Api(app)
 
 # Setup the Api resource routing here
 # Route the URL to the resource
-valid_transactioncode = ["A", "P", "S", "M"]
-data = quandl.get_table("SHARADAR/SF2", paginate=True)
-data = data.dropna(subset=["transactiondate"])
-data = data.sort_values(["transactiondate"], ascending=False)
-data = data[data["transactioncode"].isin(valid_transactioncode)]
-symbols = data["ticker"].unique()
-ticker2name = data[["ticker", "issuername"]].drop_duplicates().\
-                set_index("ticker").to_dict()["issuername"]
-
-dict_frames = {}
-amount_negotiated_avg = pd.DataFrame()
-for symbol in symbols:
-    curr_table = pd.pivot_table(data[data["ticker"]==symbol], 
-                                values=["transactionshares", 
-                                        "transactionpricepershare", 
-                                        "transactionvalue"], 
-                                index=['transactiondate'],
-                                columns=['transactioncode'], 
-                                aggfunc={"transactionshares": np.sum, 
-                                        "transactionpricepershare": np.mean,
-                                        "transactionvalue": np.sum})
-    curr_table.columns = [code + "_" + transaction for transaction, code in curr_table.columns]
-    serie = yf.Ticker(symbol)
-    stock_df = serie.history(start=data["transactiondate"].min(), 
-                             end=data["transactiondate"].max(), 
-                             interval="1d")
-    dict_frames[symbol] = stock_df.merge(curr_table, how="left", left_index=True, right_index=True)
-    
-    dict_frames[symbol]["Amount_negotiated"] = dict_frames[symbol]["Volume"]*dict_frames[symbol]["Close"]
-    dict_frames[symbol]["Amount_negotiated_MA"] = dict_frames[symbol]["Amount_negotiated"].rolling(5).mean().shift(1)
-    dict_frames[symbol]["Perc_amount_vs_MA"] = dict_frames[symbol]["Amount_negotiated"]\
-                                                /dict_frames[symbol]["Amount_negotiated_MA"]
-    if len(amount_negotiated_avg)<1:
-        amount_negotiated_avg[symbol] = dict_frames[symbol]["Amount_negotiated"]
-    else:
-        amount_negotiated_avg[symbol] = dict_frames[symbol]["Amount_negotiated"]
-
-amount_negotiated_avg = amount_negotiated_avg.mean(axis=1)
-for key, val in dict_frames.items():
-    dict_frames[key]["Perc_amount_sp100"] = dict_frames[key]["Amount_negotiated"]/amount_negotiated_avg    
+data_filings_path = os.path.join(os.getcwd(), "data", "filings")
+data = read_all_form_4(data_filings_path)
+ticker2name = create_ticker2name(data)
+dict_frames = create_combined_data(data)
 
 api.add_resource(PlotGenerator, '/generate_plot', resource_class_kwargs={'dict_frames':dict_frames, 
                                                                     'ticker_name':ticker2name})
--- api/resources.py
+++ api/resources.py
@@ -8,8 +8,7 @@ from flask import send_file
 import sys
 
 #from evaluate import Eval
-sys.path.insert(0, '../')
-print(sys.path)
+from api_utils import visualization_insider_stock
 
 parser = reqparse.RequestParser()
 parser.add_argument('query')
@@ -26,82 +25,12 @@ class PlotGenerator(Resource):
         self.dict_frames = dict_frames
         self.ticker_name = ticker_name
 
-    def post(self):
+    def get(self):
         # use parser and find the user's query
         parser = reqparse.RequestParser()
         parser.add_argument('ticker', type=str, required=True, help='Company ticker')
-
         args = parser.parse_args()
         ticker = args['ticker']
         val = self.dict_frames[ticker]
-        val = val.drop(columns=["Dividends", "Stock Splits"])
-        matplotlib.use('agg')
-        fig, ax = plt.subplots(2, 1, figsize=(20, 12))
-        ax1 = ax[0]
-        ax4 = ax[1]
-        ax2 = ax1.twinx()
-        ax3 = ax1.twinx()
-        ax3.spines["right"].set_position(("axes", 1.055))
-        # Having been created by twinx, par2 has its frame off, so the line of its
-        # detached spine is invisible.  First, activate the frame but make the patch
-        # and spines invisible.
-        make_patch_spines_invisible(ax3)
-        # Second, show the right spine.
-        ax3.spines["right"].set_visible(True)
-        ax1.plot(val["Close"], color="k")
-        ax1.errorbar(val.index, val["Close"], yerr=[val["Close"]-val["Low"], val["High"]-val["Close"]], fmt=".",
-                     color="k", ecolor='y', capthick=2, label="Close Price (Low/High error)")
-        if "S_transactionpricepershare" in val.columns:
-            ax1.plot(val.index, val["S_transactionpricepershare"],
-                     "o", color="g", label="Sell insider price")
-        if "P_transactionpricepershare" in val.columns:
-            ax1.plot(val.index, val["P_transactionpricepershare"],
-                     "o", color="r", label="Purchase insider price")
-        if "M_transactionpricepershare" in val.columns:
-            ax1.plot(val.index, val["M_transactionpricepershare"], "o",
-                     color="b", label="Derivative Conversion insider price")
-        ax1.legend(loc=2)
-        #ax2.bar(val.index, val["Volume"], 1, alpha=0.1, color="b", label="Volume")
-        if "S_transactionshares" in val.columns:
-            ax2.bar(val.index, val["S_transactionshares"], 1,
-                    alpha=0.5, color="g", label="Sell insider")
-            ax3.plot(val.index, np.abs(val["S_transactionshares"]/val["Volume"]),
-                     "x", color="g", label="Sell relative volume")
-        if "P_transactionshares" in val.columns:
-            ax2.bar(val.index, val["P_transactionshares"], 1,
-                    alpha=0.5, color="r", label="Purchase insider")
-            ax3.plot(val.index, np.abs(val["P_transactionshares"]/val["Volume"]),
-                     "x", color="r", label="Purchase relative volume")
-        if "A_transactionshares" in val.columns:
-            ax2.bar(val.index, val["A_transactionshares"], 1,
-                    alpha=0.3, color="k", label="Award insider")
-            ax3.plot(val.index, np.abs(val["A_transactionshares"]/val["Volume"]),
-                     "x", alpha=0.3, color="k", label="Award relative volume")
-        if "M_transactionshares" in val.columns:
-            ax2.bar(val.index, val["M_transactionshares"], 1, alpha=0.5,
-                    color="b", label="Derivative Conversion insider")
-            ax3.plot(val.index, np.abs(val["M_transactionshares"]/val["Volume"]),
-                     "x", color="b", label="Derivative relative volume")
-        ax2.legend(loc=1)
-        ax3.legend(loc=3)
-        #plt.boxplot(data, positions=x, notch=True)
-        ax4_color = "steelblue"
-        ax4.plot(val["Perc_amount_vs_MA"]*100, color=ax4_color,
-                 label="%Amount traded relative to MA 5 last days")
-        ax4.plot(val["Perc_amount_sp100"]*100, linestyle="--",
-                 color=ax4_color, label="%Amount relative to SP100")
-
-        ax1.set_ylabel("Price")
-        ax2.set_ylabel("Volume")
-        ax3.set_ylabel("Relative Volume")
-        ax4.set_ylabel("% Percentage")
-
-        # ax3.yaxis.label.set_color(ax3_color)
-        #ax3.tick_params(axis='y', colors=ax3_color)
-        ax4.legend(loc=2)
-        #plt.boxplot(data, positions=x, notch=True)
-        plt.title(self.ticker_name[ticker])
-        plt.savefig('plot.png')
-      
-
-        return send_file(os.getcwd() + '/plot.png', as_attachment=True)
+        visualization_insider_stock(ticker, val, self.ticker_name, save_path="insider_plot.png", from_api=True)     
+        return send_file(os.getcwd() + '/insider_plot.png', as_attachment=True)
